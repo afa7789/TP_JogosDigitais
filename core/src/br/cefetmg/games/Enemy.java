48704d80955025f572def2db520b725b0f83c909
pbelisario <pedrobelisario1997@gmail.com>
2017-12-04 20:36:16 -0200

    Criado o rabisco do inimigo, tem que tirar as texturas ainda

--- core/src/br/cefetmg/games/Enemy.java
+++ core/src/br/cefetmg/games/Enemy.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder;
 import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder.Metrics;
 import com.badlogic.gdx.ai.pfa.indexed.IndexedGraph;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import java.util.Iterator;
 
@@ -34,14 +35,16 @@ public class Enemy {
     private Facing facing;
     private TileNode nextNode, currentNode;
     public Vector2 Goal;
-    public Color color;
+    public Strength color;
     public boolean shouldMove;
     public boolean terminouOPercurso;
     public double life;
     public boolean desenhe =true;
     public boolean jaDeuPontos=false;
+    public static Vector2 worldDimensions;
+    public static final float reajuste = 0.02f;
 
-    public Enemy(Vector2 position, Color color) {
+    public Enemy(Vector2 position, Strength color,float worldWidth, float worldHeight) {
         this.position = new Position(position);
         this.color = color;
         this.steeringTarget = new Target(position);
@@ -53,11 +56,30 @@ public class Enemy {
         this.facing = Facing.EAST;
         this.shouldMove = false;
         this.terminouOPercurso = false;
-        this.life=100;
+        this.life = 14 * getSustain();
         this.desenhe = true;
         this.fullSpeed = 75;
+        this.worldDimensions = new Vector2(worldWidth, worldHeight);
+
     }
-    
+    private int getSustain() {
+         switch (color){
+             case VIOLETA:
+                 return 7;
+             case LARANJA:
+                 return 2;
+             case AMARELO:
+                 return 3;
+             case VERDE:
+                 return 4;
+             case CIANO:
+                 return 5;
+             case AZUL:
+                 return 6;
+             default:
+                 return 1;
+         }
+    }
     public void naoDesenhar(){
         desenhe = false;
     }
@@ -196,4 +218,47 @@ public class Enemy {
     public double getLife(){
         return this.life;
     }
+    public Color getColor(){
+        Color corReceber= new Color();
+        switch(color){
+            case VERMELHO:
+                corReceber.set( 1, 0, 0, 0);
+                break;
+            case LARANJA:
+                corReceber.set( 1, 0.64f, 0, 0);
+                break;
+            case AMARELO:
+                corReceber.set( 1, 1, 0, 0);
+                break;
+            case VERDE:
+                corReceber.set( 0, 1, 0, 0);
+                break;
+            case CIANO: 
+                corReceber.set( 0, 1, 1, 0);
+                break;
+            case AZUL:
+                corReceber.set( 0, 0, 1, 0);
+                break;
+            case VIOLETA:
+                corReceber.set( 0.30f , 0.18f, 0.30f, 0);
+                break;
+            }
+        return corReceber;
+    }
+    
+    public ShapeRenderer getForm (ShapeRenderer renderer) {
+        renderer.x(position.coords,10);
+        renderer.circle(position.coords.x, position.coords.y, 6);
+        Vector2 size = new Vector2(worldDimensions.cpy().scl(reajuste));
+        renderer.polygon(new float[] {      
+                    position.coords.cpy().scl(1.02f).sub(size).x, position.coords.cpy().sub(size).y,               // Vertex 0         3  
+                    position.coords.cpy().scl(0.98f).add(size).x, position.coords.cpy().sub(size).y,               // Vertex 1      /     \        
+                    position.coords.cpy().x, position.coords.cpy().add(size).scl(1.03f).y,                         // Vertex 3      \     /
+                    position.coords.cpy().scl(1.01f).sub(size).x, position.coords.cpy().scl(0.98f).add(size).y,     // Vertex 4       0---1 
+                    position.coords.cpy().scl(0.99f).add(size).x, position.coords.cpy().scl(0.98f).add(size).y    // Vertex 2     4       2   
+                    
+                });
+        return renderer;
+    }
+    
 }
